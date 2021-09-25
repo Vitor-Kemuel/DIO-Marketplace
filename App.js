8aec047096693f7f44235b89cb986ea22f3f19d4
Vitor-Kemuel <kemuvitor@gmail.com>
2021-09-25 00:38:31 -0300

    Iniciando o projeto

---
+++ App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { StyleSheet, Text, View } from 'react-native';
+
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+export default function App() {
+  return (
+    <>
+      <View style={styles.sectionContainer}>
+        <Text style={ styles.sectionDescription }>
+          read the docs to discover what to do next :
+        </Text>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
