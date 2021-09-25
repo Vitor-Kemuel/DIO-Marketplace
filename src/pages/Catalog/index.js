56b323aecad0f6595e22c0c05afe28d2676d484b
Vitor-Kemuel <kemuvitor@gmail.com>
2021-09-25 07:06:17 -0300

    iniciando estilização com Styled Components

--- src/pages/Catalog/index.js
+++ src/pages/Catalog/index.js
@@ -1,29 +1,14 @@
 import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+import { Container } from './styles';
 
-import { StyleSheet, Text, View } from 'react-native';
 
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5f5f5',
-  },
-  title: {
-    fontSize: 24,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
+ 
 
 export default function Catalog() {
   return (
     <>
-      <View style={styles.container}>
-        <Text style={ styles.title }>Catálogo</Text>
-      </View>
+      <Container />
     </>
   );
 };
