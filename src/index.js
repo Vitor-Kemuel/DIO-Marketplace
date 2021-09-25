0897a7667259a8f448dbab4fb545f2bb206295e6
Vitor-Kemuel <kemuvitor@gmail.com>
2021-09-25 06:28:02 -0300

    adicionando rota para o catálogo

--- src/index.js
+++ src/index.js
@@ -1,29 +1,7 @@
 import React from 'react';
 
-import { StyleSheet, Text, View } from 'react-native';
+import Routes from './routes';
 
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+const App = () => <Routes />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5f5f5',
-  },
-  title: {
-    fontSize: 24,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
-
-export default function App() {
-  return (
-    <>
-      <View style={styles.container}>
-        <Text style={ styles.title }>Hello, world!</Text>
-      </View>
-    </>
-  );
-};
+export default App;
